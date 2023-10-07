c6f4f965ceeecae53a8ca826a2e5a699e07a9661
abcmehedi5 <abcmehedi5@gmail.com>
2023-10-07 11:18:39 +0600

    db connect and project schema and interface

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProjectController } from './project/project.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017', {
+      dbName: 'portfolio',
+    }),
+  ],
+  controllers: [AppController, ProjectController],
   providers: [AppService],
 })
 export class AppModule {}

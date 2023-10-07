7ac8bd07e648a4dabe6b4adc3ef9cfca2d460744
abcmehedi5 <abcmehedi5@gmail.com>
2023-10-07 11:26:47 +0600

    projects module and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProjectController } from './project/project.controller';
+import { projectSchema } from './schema/project.schema';
+import { ProjectModule } from './project/project.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017', {
       dbName: 'portfolio',
     }),
+    MongooseModule.forFeature([{ name: 'Project', schema: projectSchema }]),
+    ProjectModule,
   ],
   controllers: [AppController, ProjectController],
   providers: [AppService],
